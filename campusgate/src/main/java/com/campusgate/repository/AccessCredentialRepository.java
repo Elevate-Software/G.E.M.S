e6e9b4557a61409c75c0b38133b3b9b0a0c7de0a
peter <peterkinfe548@gmail.com>
2026-06-27 21:38:30 +0300

    access credential repository

---
+++ campusgate/src/main/java/com/campusgate/repository/AccessCredentialRepository.java
@@ -0,0 +1,11 @@
+package com.campusgate.repository;
+
+import com.campusgate.entity.AccessCredential;
+import com.campusgate.entity.CredentialStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AccessCredentialRepository extends JpaRepository<AccessCredential, Long> {
+    Optional<AccessCredential> findByAuthTokenAndStatus(String authToken, CredentialStatus status);
+    Optional<AccessCredential> findByUserIdAndStatus(Long userId, CredentialStatus status);
+}
