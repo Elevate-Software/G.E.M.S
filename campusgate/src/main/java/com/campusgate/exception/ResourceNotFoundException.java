4a017fb3bec3d68405c6451893750ad51b1dc211
peter <peterkinfe548@gmail.com>
2026-07-07 15:34:56 +0300

    added custom exceptions

---
+++ campusgate/src/main/java/com/campusgate/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.campusgate.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
